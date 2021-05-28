69f53bc352c9d9ed98f37c47e5b519718a0ff1db
xSuperSunGx <noah.glatzl@gmail.com>
2021-05-28 12:37:41 +0200

    Initial Commit

---
+++ src/test/java/net/noelli/network/springframework/SpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package net.noelli.network.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
